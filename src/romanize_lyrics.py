f9d2016a21e0a54d22d3a0e396aaab218b0e5753
TeresaGarcia <teresagar181@gmail.com>
2020-06-08 15:32:14 +0200

    Capitalize first letter in line and add newlines correctly
    Signed-off-by: TeresaGarcia <teresagar181@gmail.com>

--- src/romanize_lyrics.py
+++ src/romanize_lyrics.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 
 from mykoreanromanizer import Romanizer
 
 input_file = sys.argv[1]
 r = Romanizer()
-output_file = "data/romanized/romanization.txt"
+output_file = input_file.replace("original", "romanized")
 final_text = ""
 
 with open(input_file, 'r', encoding="utf-8") as reader:
     for line in reader.readlines():
-        final_text += line + r.romanize(line) + "\n"
+        clean_line = line.rstrip()
+        rom_line = r.romanize(clean_line)
+        rom_line = rom_line[:1].upper() + rom_line[1:]
+        final_text += clean_line + "\n" + rom_line + "\n\n"
 
 with open(output_file, 'w', encoding="utf-8") as writer:
     writer.writelines(final_text)
-
-print(frase)
