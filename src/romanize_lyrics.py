383311e2bc9ee3e1d3d18dbe578a4a8b1a6bbdf5
TeresaGarcia <teresagar181@gmail.com>
2020-06-08 15:11:36 +0200

    Basic functionality done
    Signed-off-by: TeresaGarcia <teresagar181@gmail.com>

--- src/romanize_lyrics.py
+++ src/romanize_lyrics.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
 from mykoreanromanizer import Romanizer
 
+input_file = sys.argv[1]
 r = Romanizer()
+output_file = "data/romanized/romanization.txt"
+final_text = ""
 
-frase = r.romanize("살고 싶어")
+with open(input_file, 'r', encoding="utf-8") as reader:
+    for line in reader.readlines():
+        final_text += line + r.romanize(line) + "\n"
+
+with open(output_file, 'w', encoding="utf-8") as writer:
+    writer.writelines(final_text)
 
 print(frase)
